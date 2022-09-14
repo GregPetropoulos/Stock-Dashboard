d635fd3c1f27542caef587d21e58f00632df922c
GP <gregpetropoulos@yahoo.com>
2022-09-14 07:51:35 -0400

    Added icons and responsive side-navbar  and header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import { BiSearch } from 'react-icons/bi';
 
 const Header = () => {
   return (
-    <div className='block m-3  xsm:flex xsm:flex-row xsm:justify-center'>
-      <div className='flex flex-col align-middle w-full justify-start '>
-        <h1 className=' text-base font-bold xsm:text-xl'>Dashboard</h1>
-        <p>10:00 Am * 30 May 2022</p>
+    <div className='block text-center p-0 m-0 xsm:flex xsm:flex-row xsm:justify-center'>
+      <div className=' xsm:flex xsm:flex-col xsm:align-middle xsm:w-full xsm:justify-start  '>
+        <h1 className=' text-base font-bold  xsm:text-xl'>Dashboard</h1>
+        <p className='text-xs sm:text-md'>10:00 Am * 30 May 2022</p>
       </div>
 
-      <div className='flex flex-col w-full align-middle justify-center'>
-        <input
-          type='text'
-          placeholder='Search here'
-          className=' input input-xs input-bordered input-success w-32'
-        />
+      <div className=' hidden sm:flex xsm:flex-col sm:w-full sm:align-middle sm:justify-center'>
+        <div className='xsm:flex-row xsm:flex xsm:relative'>
+          <input
+            type='text'
+            placeholder='Search here'
+            className='  xsm:p-3 xsm:input xsm:input-xs xsm:input-bordered xsm:input-success xsm:w-32 xsm:mr-1'
+          />
+          <BiSearch className=' xsm:absolute xsm:pointer-events-none xsm:left-24 xsm:top-1'/>
+        </div>
       </div>
     </div>
   );
--- client/src/components/SideNav.jsx
+++ client/src/components/SideNav.jsx
@@ -1,28 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { BiMessageSquareAdd } from 'react-icons/bi';
+import { BiPurchaseTag } from 'react-icons/bi';
+import { BiWallet } from 'react-icons/bi';
+import { AiOutlineSetting } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
 
 const SideNav = () => {
   return (
-    <div className='w-full'>
-            <div className='flex flex-col justify-center align-middle '>
-        {/* <label htmlFor='my-drawer-1' className='drawer-overlay'></label> */}
-        <ul className='menu border-r-2 h-full border-r-success p-4 overflow-y-auto w-32 bg-base-100 text-base-content'>
-          {/* <!-- Sidebar content here --> */}
-          <li className='m-4 text-center'>
-            <a  href={'!#'} alt='wallet' className=''>Wallet</a>
-          </li>
-          <li className='m-4'>
-            <a href={'!#'} alt='Trades' className=''>Trades</a>
-          </li>
-          <li className='m-4 text-center'>
-            <a href={'!#'} alt='Message' className=''>Messages</a>
-          </li>
-          <li className='m-4'>
-            <a href={'!#'} alt='Settings' className=''>Settings</a>
-          </li>
-        </ul>
-      </div>
+    <div className='block mt-3 text-center xsm:flex xsm:flex-col xsm:w-full xsm:justify-center xsm:align-middle xsm:h-screen '>
+      {/* <label htmlFor='my-drawer-1' className='drawer-overlay'></label> */}
+      <ul className=' menu xsm:justify-evenly xsm:border-r-2 xsm:h-full xsm:border-r-success xsm:p-1 xsm:overflow-y-auto xsm:w-36 bg-base-100 text-base-content'>
+        {/* <!-- Sidebar content here --> */}
+        <li className='  mx-4  text-sm sm:hidden'>
+          <a href={'!#'} alt='search' className='flex justify-center'>
+            <BiSearch className='mr-2 mt-1' />
+            Search
+          </a>
+        </li>
+        <li className='mx-4 text-sm'>
+          <a
+            href={'!#'}
+            alt='wallet'
+            className='flex justify-center'>
+            <BiWallet className='mr-2 mt-1' />
+            <p className=''>Wallet</p>
+          </a>
+        </li>
+        <li className='mx-4  text-sm'>
+          <a href={'!#'} alt='Trades' className='flex justify-center'>
+            <BiPurchaseTag className='mr-2 mt-1' />
+            Trades
+          </a>
+        </li>
+        <li className='mx-4  text-sm'>
+          <a href={'!#'} alt='Message' className='flex justify-center'>
+            <BiMessageSquareAdd className='mr-2 mt-1' />
+            Messages
+          </a>
+        </li>
+        <li className='mx-4  text-sm'>
+          <a href={'!#'} alt='Settings' className='flex justify-center'>
+            <AiOutlineSetting className='mr-2 mt-1' />
+            Settings
+          </a>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default SideNav
+export default SideNav;
